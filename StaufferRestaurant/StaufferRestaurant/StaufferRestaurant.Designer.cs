d2a9af649c4e1503f4f3ad59db2ff137da373636
resistince <travisrgstauffer@gmail.com>
2015-04-09 21:52:27 -0400

    Started form design
    Added a menu strip with some options. All have keyboard access and code
    behind them

---
+++ StaufferRestaurant/StaufferRestaurant/StaufferRestaurant.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace StaufferRestaurant
+{
+    partial class frmStaufferRestaurant
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmStaufferRestaurant));
+            this.mnuMainStrip = new System.Windows.Forms.MenuStrip();
+            this.mnuFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuFileClose = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuFileExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuWindow = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuWindowCascade = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuWindowTile = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuWindowTileHorizontal = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuWindowTileVertical = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuMainStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mnuMainStrip
+            // 
+            this.mnuMainStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuFile,
+            this.mnuWindow});
+            this.mnuMainStrip.Location = new System.Drawing.Point(0, 0);
+            this.mnuMainStrip.Name = "mnuMainStrip";
+            this.mnuMainStrip.Size = new System.Drawing.Size(1352, 24);
+            this.mnuMainStrip.TabIndex = 1;
+            // 
+            // mnuFile
+            // 
+            this.mnuFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuFileClose,
+            this.mnuFileExit});
+            this.mnuFile.Name = "mnuFile";
+            this.mnuFile.Size = new System.Drawing.Size(37, 20);
+            this.mnuFile.Text = "&File";
+            // 
+            // mnuFileClose
+            // 
+            this.mnuFileClose.Name = "mnuFileClose";
+            this.mnuFileClose.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.C)));
+            this.mnuFileClose.Size = new System.Drawing.Size(152, 22);
+            this.mnuFileClose.Text = "&Close";
+            this.mnuFileClose.Click += new System.EventHandler(this.mnuFileClose_Click);
+            // 
+            // mnuFileExit
+            // 
+            this.mnuFileExit.Name = "mnuFileExit";
+            this.mnuFileExit.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F4)));
+            this.mnuFileExit.Size = new System.Drawing.Size(152, 22);
+            this.mnuFileExit.Text = "&Exit";
+            this.mnuFileExit.Click += new System.EventHandler(this.mnuFileExit_Click);
+            // 
+            // mnuWindow
+            // 
+            this.mnuWindow.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuWindowCascade,
+            this.mnuWindowTile});
+            this.mnuWindow.Name = "mnuWindow";
+            this.mnuWindow.Size = new System.Drawing.Size(63, 20);
+            this.mnuWindow.Text = "&Window";
+            // 
+            // mnuWindowCascade
+            // 
+            this.mnuWindowCascade.Name = "mnuWindowCascade";
+            this.mnuWindowCascade.Size = new System.Drawing.Size(152, 22);
+            this.mnuWindowCascade.Text = "C&ascade";
+            this.mnuWindowCascade.Click += new System.EventHandler(this.mnuWindowCascade_Click);
+            // 
+            // mnuWindowTile
+            // 
+            this.mnuWindowTile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuWindowTileHorizontal,
+            this.mnuWindowTileVertical});
+            this.mnuWindowTile.Name = "mnuWindowTile";
+            this.mnuWindowTile.Size = new System.Drawing.Size(152, 22);
+            this.mnuWindowTile.Text = "&Tile";
+            // 
+            // mnuWindowTileHorizontal
+            // 
+            this.mnuWindowTileHorizontal.Name = "mnuWindowTileHorizontal";
+            this.mnuWindowTileHorizontal.Size = new System.Drawing.Size(129, 22);
+            this.mnuWindowTileHorizontal.Text = "&Horizontal";
+            this.mnuWindowTileHorizontal.Click += new System.EventHandler(this.mnuWindowTileHorizontal_Click);
+            // 
+            // mnuWindowTileVertical
+            // 
+            this.mnuWindowTileVertical.Name = "mnuWindowTileVertical";
+            this.mnuWindowTileVertical.Size = new System.Drawing.Size(129, 22);
+            this.mnuWindowTileVertical.Text = "&Vertical";
+            this.mnuWindowTileVertical.Click += new System.EventHandler(this.mnuWindowTileVertical_Click);
+            // 
+            // frmStaufferRestaurant
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1352, 612);
+            this.Controls.Add(this.mnuMainStrip);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.mnuMainStrip;
+            this.MinimumSize = new System.Drawing.Size(1358, 620);
+            this.Name = "frmStaufferRestaurant";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Stauffer Restaurant Reservation System";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.mnuMainStrip.ResumeLayout(false);
+            this.mnuMainStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip mnuMainStrip;
+        private System.Windows.Forms.ToolStripMenuItem mnuFile;
+        private System.Windows.Forms.ToolStripMenuItem mnuFileClose;
+        private System.Windows.Forms.ToolStripMenuItem mnuFileExit;
+        private System.Windows.Forms.ToolStripMenuItem mnuWindow;
+        private System.Windows.Forms.ToolStripMenuItem mnuWindowCascade;
+        private System.Windows.Forms.ToolStripMenuItem mnuWindowTile;
+        private System.Windows.Forms.ToolStripMenuItem mnuWindowTileHorizontal;
+        private System.Windows.Forms.ToolStripMenuItem mnuWindowTileVertical;
+    }
+}
+
---
+++ StaufferRestaurant/StaufferRestaurant/StaufferRestaurant.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StaufferRestaurant
+{
+    public partial class frmStaufferRestaurant : Form
+    {
+        public frmStaufferRestaurant()
+        {
+            InitializeComponent();
+        }
+
+        private void mnuWindowTileHorizontal_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(MdiLayout.TileHorizontal);
+        }
+
+        private void mnuWindowTileVertical_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(MdiLayout.TileVertical);
+        }
+
+        private void mnuWindowCascade_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(MdiLayout.Cascade);
+        }
+
+        private void mnuFileClose_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ActiveMdiChild.Close();
+            }
+            catch (NullReferenceException nre)
+            {
+                MessageBox.Show("No forms to close", "Close Error");
+            }
+        }
+
+        private void mnuFileExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
